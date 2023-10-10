c18835fbd65b305aa6d85e4ca50f452da13efdb7
pdiggidy <p.c.nierop@student.tue.nl>
2023-10-10 17:26:33 +0200

    added filtering for just the central component (in this case that means they are connected to "VOLKSWAGEN")

--- filtered_automotive.R
+++ filtered_automotive.R
@@ -1,3 +1,16 @@
+#install.packages("patentsview")
+library(tidyr)
+library(dplyr)
+library(igraph)
+library(data.table)
+library(patentsview) #this package is needed for retrieving the data
+library(tidyr) #this package is needed for quickly subsetting the data
+#install.packages("devtools")
+#devtools::install_github("stasvlasov/nstandr")
+library(nstandr)
+install.packages("networkD3")
+library(networkD3)
+sdc_data <- readRDS("SDC_data_2021.rds")
 
 automotive = sdc_data %>% filter(status == "Completed/Signed",
                                  #type == "Strategic Alliance",
@@ -53,3 +66,41 @@ interactive_graph <- forceNetwork(Links = automotive_deals_net_d3$links,
 interactive_graph
 
 V(automotive_deals_graph)$name
+
+#volkswagen
+
+# Assuming your "volkswagen" node is identified by its name, replace "Volkswagen" with the actual name.
+target_node_name <- "Volkswagen"
+
+# Find the index of the "volkswagen" node in the graph
+target_node_index <- which(V(automotive_deals_graph)$participants == target_node_name)
+
+# Perform a breadth-first search starting from the "volkswagen" node
+# to identify all connected nodes.
+connected_nodes <- bfs(automotive_deals_graph, root = target_node_index, order = TRUE)$order
+
+# Create a subgraph containing only the "volkswagen" network
+subgraph <- induced_subgraph(automotive_deals_graph, vids = connected_nodes)
+
+# Plot the subgraph
+plot(subgraph, layout = coords, vertex.color = "coral2",
+     vertex.size = 1, edge.width = 0.002,
+     edge.color = adjustcolor("black", alpha.f = 0.1),
+     vertex.label = V(subgraph)$name, vertex.label.cex = 0.01)
+
+# Convert the subgraph to a networkD3 object and visualize it
+subgraph_net_d3 <- igraph_to_networkD3(subgraph, group = V(subgraph)$name)
+interactive_subgraph <- forceNetwork(Links = subgraph_net_d3$links,
+                                     Nodes = subgraph_net_d3$nodes,
+                                     Source = 'source',
+                                     Target = 'target',
+                                     NodeID = 'name',
+                                     Group = 'group',
+                                     zoom = TRUE,
+                                     linkColour = "black",
+                                     fontFamily = "Times New Roman",
+                                     fontSize = 11,
+                                     opacity = 0.9)
+
+interactive_subgraph
+
